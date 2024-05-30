45dbf9ca67bc4afe5d8ce452e31ebb343b59e503
Enddy123 <140968646+Enddy123@users.noreply.github.com>
2024-05-30 08:05:59 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -14,8 +14,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # Other fields as needed
-    
+  
     def __str__(self):
         return self.name  # Return the name as the string representation
 
