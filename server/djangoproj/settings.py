e0e2bd07b1545d90113ece9d86767e79ab4f3551
Enddy123 <140968646+Enddy123@users.noreply.github.com>
2024-05-30 16:42:41 +0200

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -72,9 +72,9 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(BASE_DIR, 'frontend/static'),
-                  os.path.join(BASE_DIR, 'frontend/build'),
-                  os.path.join(BASE_DIR, 'frontend/build/static'),
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/static'),
+                 os.path.join(BASE_DIR, 'frontend/build'),
+                 os.path.join(BASE_DIR, 'frontend/build/static'),
                 ],
         
         'APP_DIRS': True,
