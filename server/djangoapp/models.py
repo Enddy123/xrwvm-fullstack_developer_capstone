8b708bf19e4d88296da347126aa1c6c0cc8f6b63
Enddy123 <140968646+Enddy123@users.noreply.github.com>
2024-05-30 09:01:58 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -14,10 +14,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    
     def __str__(self):
         return self.name  # Return the name as the string representation
-        pass
+        
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
