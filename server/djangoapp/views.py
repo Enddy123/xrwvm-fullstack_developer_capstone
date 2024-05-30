261637162f525f469d84c413395d61ae8133d3ab
Enddy123 <140968646+Enddy123@users.noreply.github.com>
2024-05-30 13:49:08 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -72,14 +72,14 @@ def registration(request):
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
-                                        password=password, email=email)
+        user = User.objects.create_user(username=username, first_name=first_name,
+                                        last_name=last_name, password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName" : username, "status" :" Authenticated"}
+        data = {"userName" : username, "status" : "Authenticated"}
         return JsonResponse(data)
     else :
-        data = {"userName":username,"error":"Already Registered"}
+        data = {"userName" : username, "error" : "Already Registered"}
         return JsonResponse(data)
 
 # # Update the `get_dealerships` view to render the index page with
